fb528f83edf67c1581bc2ba8f018a90d3cc9ffd0
KeithCressman <77999322+KeithCressman@users.noreply.github.com>
2021-04-19 20:07:33 -0400

    Update and rename layers.py to model.py

--- model.py
+++ model.py
@@ -45,3 +45,27 @@ class GraphConvolution(Module):
         return self.__class__.__name__ + ' (' \
                + str(self.in_features) + ' -> ' \
                + str(self.out_features) + ')'
+    
+   
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GCN(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout):
+        super(GCN, self).__init__()
+
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nclass)
+        self.dropout = dropout
+
+    def forward(self, x, adj):
+        #print(x.shape,'x')
+        #print(adj.shape,'adj')
+        x = F.relu(self.gc1(x, adj))
+        embeddings = x
+        x = F.dropout(x, self.dropout, training=self.training)
+
+        x = self.gc2(x, adj)
+        return F.log_softmax(x, dim=1)
